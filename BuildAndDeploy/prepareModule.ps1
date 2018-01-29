8caa613c6ba2da671749a95fb5c33b595df6cea8
Sean McNellis <seanmcne@hotmail.com>
2018-01-29 16:40:00 -0600

    Update prepareModule.ps1

--- BuildAndDeploy/prepareModule.ps1
+++ BuildAndDeploy/prepareModule.ps1
@@ -40,5 +40,5 @@ Catch{
 
 $Cert = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2($keypath,$keypass)
 
-Set-AuthenticodeSignature -Certificate $Cert -TimeStampServer http://timestamp.verisign.com/scripts/timstamp.dll -FilePath "$ModuleName\$ModuleName.psd1"
-Set-AuthenticodeSignature -Certificate $Cert -TimeStampServer http://timestamp.verisign.com/scripts/timstamp.dll -FilePath "$ModuleName\$ModuleName.psm1"
+Set-AuthenticodeSignature -Certificate $Cert -TimeStampServer http://timestamp.verisign.com/scripts/timstamp.dll -FilePath "$ModuleName.psd1"
+Set-AuthenticodeSignature -Certificate $Cert -TimeStampServer http://timestamp.verisign.com/scripts/timstamp.dll -FilePath "$ModuleName.psm1"
